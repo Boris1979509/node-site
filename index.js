77336b54911d26a334757d63cbc22f1f660ba6fb
Boris <bobabonanadz@mail.ru>
2020-10-17 13:38:56 +0300

    Install pug, add views, add static public

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
+
 const app = express();
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('pages/home', {title: 'Home page', isHome: true});
+});
+app.get('/courses', (req, res) => {
+    res.render('pages/courses', {title: 'Courses page', isCourses: true});
+});
+app.get('/add', (req, res) => {
+    res.render('pages/add', {title: 'Add page', isAdd: true});
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
