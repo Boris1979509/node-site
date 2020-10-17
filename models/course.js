72080202e5abfa5534e5b85d5782bd51892303d8
Boris <bobabonanadz@mail.ru>
2020-10-17 17:06:26 +0300

    Add model Course

---
+++ models/course.js
@@ -0,0 +1,55 @@
+const uuid = require('uuid'); // unique ids
+const fs = require('fs');
+const path = require('path');
+
+class Course {
+    constructor(title, price, image) {
+        this.title = title;
+        this.price = price;
+        this.image = image;
+        this.id = uuid.v4();
+    }
+
+    toJson() {
+        return {
+            title: this.title,
+            price: this.price,
+            image: this.image,
+            _id: this.id,
+        };
+    }
+
+    async save() {
+        const courses = await Course.getAll();
+        courses.push(this.toJson());
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+        });
+    }
+
+    static getAll() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                            resolve(JSON.parse(content));
+                    }
+                });
+        });
+    }
+}
+
+module.exports = Course;
--- routes/add.js
+++ routes/add.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
-
+const Course = require('../models/course');
 router.get('/', (req, res) => {
     res.render('pages/add', {title: 'Add courses', isAdd: true});
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/', async (req, res) => {
+    const course = new Course(req.body.title, req.body.price, req.body.image);
+    await course.save();
+    res.redirect('/courses');
 });
 
 module.exports = router;
